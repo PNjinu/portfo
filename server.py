6acff4ee7f9906526fcaa20aee792483d438bd5f
Patrick Kuria <kuria144@gmail.com>
2023-01-19 16:08:17 +0300

    Simple stupid, form collects data and puts it in a csv

--- server.py
+++ server.py
@@ -1,86 +1,35 @@
 from flask import Flask, render_template, request, redirect
 import os
 import csv
-from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
-app.config["SQLALCHEMY_DATABASE_URI"] = "sqlite:///database.sqlite"
-db = SQLAlchemy(app)
 
-@app.route("/")
+@app.route("/", methods=['POST', 'GET'])
 def my_home():
-    return render_template('index.html')
-
-@app.route("/<string:page_name>")
-def html_page(page_name):
-    return render_template(page_name)
-
-
-def write_to_file(data):
-    with open('database.txt', mode='a') as database:
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
-        file = database.write(f'\n{email}, {subject}, {message}') 
-
-def write_to_csv(data):
-    with open('database.csv', mode='a') as database2:
-        email = data['email']
-        subject = data['subject']
-        message = data['message']
-        csv_writer = csv.writer(database2, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
-        csv_writer.writerow([email,subject,message])
-
-@app.route('/submit_form', methods=['POST', 'GET'])
-def submit_form():
     if request.method == 'POST':
         try:
             data = request.form.to_dict()
             write_to_csv(data)
-            return redirect('/thankyou.html')
-        except:
-            return 'did not save to database'
+            message = "Thank you for sending me a message, I will get back ASAP."
+            return render_template('index.html', message=message)
+        except Exception as e:
+            message = f"Did not save to database: {e}"
+            return message
     else:
-        return 'something went wrong, try again'
+        return render_template('index.html')
 
-# Database models
-class Services(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    icon = db.Column(db.String, unique=True, nullable=False)
-    title = db.Column(db.String, unique=True, nullable=False)
-    description = db.Column(db.String, nullable=False)
+    
 
-    def __init__(self, title, description):
-        self.title = title
-        self.description = description
-
-class Portfolio(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    icon = db.Column(db.String, unique=True, nullable=False)
-    title = db.Column(db.String, unique=True, nullable=False)
-    description = db.Column(db.String, nullable=False)
-
-    def __init__(self, title, description):
-        self.title = title
-        self.description = description
-
-class Blog(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    image = db.Column(db.String, nullable=False)
-    title = db.Column(db.String, unique=True, nullable=False)
-    metadata = db.Column(db.String, unique=True, nullable=False)
-    link = db.Column(db.String, nullable=False)
-
-    def __init__(self, title, description):
-        self.title = title
-        self.description = description
-
-with app.app_context():
-    db.create_all()
-
+# This will be replaced when database is set up.
+def write_to_csv(data):
+    with open('database.csv', mode='a') as database:
+        name = data['name']
+        email = data['email']
+        subject = data['subject']
+        message = data['message']
+        csv_writer = csv.writer(database, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        csv_writer.writerow([name,email,subject,message])
 
 
 if __name__ == "__main__":
-    #app.run(Debug=True)
-    print(app)
-
+    app.run(debug=True)
